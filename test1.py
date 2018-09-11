58691b1f3c5018d2307c0aa77dc69ec51570cf13
CDFwanderer <ati@kth.se>
2018-09-11 19:55:11 +0200

    This is a new branch; testing this feature

--- test1.py
+++ test1.py
@@ -10,3 +10,6 @@ if x is True:
     print("Yadayada!")
 
 y = input("y = user's input ")
+
+# z is the third dimension
+# Gestern, Heute, Morgen, folgen nicht auf einander; sie sind in einer ewigen Kreis mitenander verbunden
