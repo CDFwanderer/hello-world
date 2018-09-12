82abb7fb40cb25dcda4831c687251de729d37cf8
CDFwanderer <37913125+CDFwanderer@users.noreply.github.com>
2018-09-12 11:13:28 +0200

    Update test1.py
    Doing some changes for test

--- test1.py
+++ test1.py
@@ -4,13 +4,6 @@ print("Hello World! Litteraly hello world :D")
 
 # commenting some more
 
-X = True
-
-if x is True:
-    print("Yadayada!")
-
-y = input("y = user's input ")
-
 class Test()
     def __init__(self):
         print("Hello I am a class!")
